90e8ba832f07f64b3f31181e7bfa1e07f0db64b7
RehanAli7024 <bb1221388@iitd.ac.in>
2024-04-18 23:27:07 +0530

    MUI Installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './pages/home';
 function App() {
   return (
     <div className="App">
-      Hello Tanishk
+      <Home></Home>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import Button from  '@mui/material/Button';
+import './home.css';
+
+const Home = ()=>{
+    return<>
+    
+        <Button id='checkButton' variant='outlined' >Button</Button>
+    </>
+}
+
+export default  Home;
